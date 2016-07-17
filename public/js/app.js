49bf7844caced75da5db0f4437bb2c290aaff8d7
Tanachi <tanachige@gmail.com>
2016-07-16 14:22:31 -1000

    update

--- public/js/app.js
+++ public/js/app.js
@@ -12,9 +12,9 @@ $.ajax({
     var myDate = new Date( time*1000);
     var $div =  $('<div>', {class:'article'});
     var $imageArea = $('<div>', {class:'imageArea'});
-    $imageArea.css( "background-image",threadData.url);
+    $imageArea.css( "background-image",'url(' + threadData.url + ')');
     var $titleArea = $('<h1>', {class:'titleArea'}).html(threadData.title);
-    var $infoArea = $('<div>', {class:'infoArea'}).html('by ' + threadData.author + '•' + 'created' + myDate + 'ago' + '•' +
+    var $infoArea = $('<div>', {class:'infoArea'}).html('by ' + threadData.author + '•' + 'created ' + myDate + ' ago' + '•' +
       threadData.score + ' views');
     $($div).append($imageArea);
     $($div).append($titleArea);
